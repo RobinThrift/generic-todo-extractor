f6d895c553e14cce49fbcd3775c4a10f72e0409c
Robin Thrift <thrift.d.robin@googlemail.com>
2015-10-06 21:39:25 +0200

    💻 basic build stuff

---
+++ test/helpers.js
@@ -0,0 +1,15 @@
+
+import Q from 'q';
+import {readFile} from 'fs';
+import {join} from 'path';
+
+var rf = Q.nfbind(readFile);
+
+export function fixtureFiles(base, ...files) {
+    var proms = [];
+    files.forEach((file) => {
+        proms.push(rf(join(__dirname, 'fixtures', base, file), 'utf8'));
+    });
+
+    return Q.all(proms);
+}
